44c85b2c45fe0d5078dc4a815f45cc54bbcaca25
Shiv10 <shivanshsharma2012@gmail.com>
2021-07-29 21:19:39 +0530

    fix: minor changes

--- gred-travller-memoization.py
+++ gred-travller-memoization.py
@@ -1,6 +1,8 @@
 def gridtraveller(n,m, d = {}):
     if (n,m) in d.keys():
         return d[(n,m)]
+    if (m, n) in d.keys():
+        return d[(m,n)]
     if n==0 or m == 0:
         return 0
     if n==1 and m ==1:
