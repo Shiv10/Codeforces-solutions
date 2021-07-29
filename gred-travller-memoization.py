db517949d675b9084a17cf3a5f38cf0d7856deba
Shiv10 <shivanshsharma2012@gmail.com>
2021-07-29 21:17:46 +0530

    feat: add grid traveller with dp

---
+++ gred-travller-memoization.py
@@ -0,0 +1,14 @@
+def gridtraveller(n,m, d = {}):
+    if (n,m) in d.keys():
+        return d[(n,m)]
+    if n==0 or m == 0:
+        return 0
+    if n==1 and m ==1:
+        return 1
+    
+    d[(n,m)] = gridtraveller(n-1,m)+gridtraveller(n,m-1)
+    return d[(n,m)]
+    
+n = int(input())
+m = int(input())
+print(gridtraveller(n,m))
