5412eebbff858d71b7f531279054e82925fc7105
Shiv10 <shivanshsharma2012@gmail.com>
2021-08-16 15:47:03 +0530

    fix: question

--- B types/B. Sereja and Suffixes.py
+++ B types/B. Sereja and Suffixes.py
@@ -1,15 +1,14 @@
 t, n = map(int, input().split())
 a = list(map(int, input().split()))
-d = {t: 1}
-b = [a[t-1]]
-for i in range(t-2, -1, -1):
-    if a[i] in b:
-        d[i+1] = d[i+2]
-    else:
-        d[i+1] = d[i+2]+1
-        b.append(a[i])
-
+d = {}
+b = [0] * len(a) 
+s = 0
+for i in range(t-1, -1, -1):
+    if d.get(a[i], 0) == 0:
+        d[a[i]] = 1
+        s += 1
+    b[i] = s
 
 for _ in range(n):
     c = int(input())
-    print(d[c])
+    print(b[c-1])
