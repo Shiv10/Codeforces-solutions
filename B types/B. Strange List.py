b50c39da92ad029eac1661d1bf7a2a88411d96e8
Shiv10 <shivanshsharma2012@gmail.com>
2021-08-18 21:16:54 +0530

    feat: add question

--- B types/B. Strange List.py
+++ B types/B. Strange List.py
@@ -1,18 +1,33 @@
+def calc(a,x,d):
+    if d.get(a, -1)!=-1:
+        return d
+    
+    c = 1
+    t = a
+    while a%x ==0:
+        c+=1
+        a//=x
+    d[t] = c
+    return d
 t = int(input())
 for _ in range(t):
     n, x = map(int, input().split())
     a = list(map(int,input().split()))
-    i = 0
+    d = {}
     c = 0
-    while (True):
-        if a[i]%x!=0:
-            break
-        c+=a[i]
-        s = a[i]//x
-        a = a[0:]+([s]*x)
-        i+=1
-
-    while(i<len(a)):
-        c+=a[i]
-        i+=1
+    ind = 0
+    temp = 0
+    m = 10**9
+    flag = False
+    for i in range(n):
+        d = calc(a[i], x, d)
+        if d[a[i]] < m:
+            m = d[a[i]]
+            ind = i
+    
+    for i in range(n):
+        if i < ind:
+            c += a[i]*(m+1)
+        else:
+            c += a[i] * m
     print(c)
