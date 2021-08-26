5ce7fdcf97fc913f66a38055c194c8223ae4f748
Shiv10 <shivanshsharma2012@gmail.com>
2021-08-26 15:05:00 +0530

    feat: add question

---
+++ A types/A_Filling_Diamonds.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t, n;
+    cin>>t;
+    while(t>0){
+        cin>>n;
+        cout<<n<<endl;
+        t--;
+    }
+    return 0;
+}
