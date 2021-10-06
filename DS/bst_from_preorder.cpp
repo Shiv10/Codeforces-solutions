4402ee338c0f31f68be6f76cea3c264fcf8c15d3
Shiv10 <shivanshsharma2012@gmail.com>
2021-10-06 13:27:59 +0530

    feat: add question

---
+++ DS/bst_from_preorder.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int val;
+    Node *left;
+    Node *right;
+};
+
+Node *getNewNode(int val){
+    Node *newNode;
+    newNode->val = val;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+int constructBst(vector<int> &preorder, int n, int pos, Node *current, int left, int right){
+    
+    if(pos==n || preorder[pos]<left || preorder[pos]>right)
+            return pos;
+        
+    if(preorder[pos]<current->val)
+    {
+        current->left = getNewNode(preorder[pos]);
+        pos += 1;
+        pos = constructBst(preorder,n,pos,current->left,left,current->val-1);
+    }
+        
+    if(pos==n || preorder[pos]<left || preorder[pos]>right)
+        return pos;
+        
+    current->right = getNewNode(preorder[pos]);
+    pos += 1;
+    pos = constructBst(preorder,n,pos,current->right,current->val+1,right);
+    return pos;
+}
+
